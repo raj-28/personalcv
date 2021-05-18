f24b9bc5b6262a58f4c22742824681d68b0f02d6
Raj <kumarraj16@gmail.com>
2021-05-18 12:06:20 +0530

    First Personal CV Commit

---
+++ personalcv/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for personalcv project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'personalcv.settings')
+
+application = get_wsgi_application()
